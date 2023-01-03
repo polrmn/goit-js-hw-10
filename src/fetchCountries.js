a478dd0ae741f260f5e9e2a77e54df77a2c29683
ROMAN POLISHCHUK <114829451+polrmn@users.noreply.github.com>
2023-01-03 14:17:42 +0200

    first commit

---
+++ src/fetchCountries.js
@@ -0,0 +1,10 @@
+// export default fetchCountries;
+export function fetchCountries(name) {
+    return fetch(`https://restcountries.com/v3.1/name/${name}`)
+        .then(response => {
+            if(!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json()
+        });
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import './css/styles.css';
+import fetchCountries from './fetchCountries';
+const refs = {
+    searchInput: document.querySelector('#search-box'),
+    countryList: document.querySelector('.country-list'),
+    countryInfo: document.querySelector('.country-info'),
+}
+let name = '';
+refs.searchInput.addEventListener('input', onInput);
+fetchCountries(name).than(response => console.log(1)) 
+
+function onInput(e) {
+    name = e.target.value;
+}
 
 const DEBOUNCE_DELAY = 300;
