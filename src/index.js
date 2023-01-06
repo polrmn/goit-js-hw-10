ff681eb1031d6627621df0b135a55e29085291af
ROMAN POLISHCHUK <114829451+polrmn@users.noreply.github.com>
2023-01-06 14:44:33 +0200

    final

--- src/index.js
+++ src/index.js
@@ -1,30 +1,69 @@
 import './css/styles.css';
-import fetchCountries from './fetchCountries';
+import debounce from 'lodash.debounce';
+import {fetchCountries} from './fetchCountries';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 const refs = {
     searchInput: document.querySelector('#search-box'),
     countryList: document.querySelector('.country-list'),
     countryInfo: document.querySelector('.country-info'),
 }
 let name = '';
-refs.searchInput.addEventListener('focus', onInput);
-const searchParam = new URLSearchParams {
-    fields: 'capital',
-}
-// ,capital,population,flags.svg,languages
+const DEBOUNCE_DELAY = 300;
+const searchParams = new URLSearchParams ({
+    fields: ['capital', 'name.official', 'population', 'flags.svg','languages'],
+});
+
+refs.searchInput.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
 function onInput(e) {
-    name = e.target.value;
-    fetch(`https://restcountries.com/v3.1/name/${name}?fields=flags.svg`).then(
-        response => {
-            if(!response.ok) {
-                throw new Error(response.status);
-            }
-            return response.json()
-        }).then( 
-            countryname =>  
-            countryname.forEach(countryInfo => {
-                console.log(countryInfo);
-            }) )
+    refs.countryList.innerHTML = '';
+    refs.countryInfo.innerHTML = '';
+    name = e.target.value.trim();  
+    fetchCountries(name).then( 
+            countryname => {
+                if (countryname.length > 10) {
+                    Notify.info('Too many matches found. Please enter a more specific name.')
+                } else if(countryname.length >= 2) {
+                    countryname.forEach(element =>  refs.countryList.insertAdjacentHTML('beforeend', 
+                    `
+                    <li class="country-item">
+                        <img 
+                            class="country-item__flag" 
+                            src="${element.flags.svg}" 
+                            alt="/"
+                        />
+                    <p class="country-name">${element.name}</p>
+                `)
+                        );
+                   
+                } else {
+                    countryname.forEach(element => refs.countryInfo.innerHTML = 
+                        `
+                            <img 
+                                class="country-info__flag" 
+                                src="${element.flags.svg}" 
+                                alt="/"
+                            />
+                            <h1 class="country-info__name">${element.name}</h1>
+                            <ul class="country-info__list">
+                                <li class="country-info__item">
+                                    <span class="country-info__tag">Capital:</span>
+                                    <p class="country-info__value">${element.capital}</p>
+                                </li>
+                                <li class="country-info__item">
+                                    <span class="country-info__tag">Population:</span>
+                                    <p class="country-info__value">${element.population}</p>
+                                </li>
+                                <li class="country-info__item">
+                                    <span class="country-info__tag">Languages:</span>
+                                    <p class="country-info__value">${element.languages.map(lang => lang.name)}</p>
+                                </li>
+                            </ul>
+                        `);                   
+                }
+                console.log(...countryname);
+
+            });
 }
 
-const DEBOUNCE_DELAY = 300;
