bfecee075471d1b34530fdca74048fd3e210a4fd
polrmn <114829451+polrmn@users.noreply.github.com>
2023-01-06 12:45:00 +0000

    Deploying to gh-pages from @ polrmn/goit-js-hw-10@ff681eb1031d6627621df0b135a55e29085291af 🚀

--- index.d2473424.js
+++
@@ -1,2 +0,0 @@
-const e={searchInput:document.querySelector("#search-box"),countryList:document.querySelector(".country-list"),countryInfo:document.querySelector(".country-info")};let t="";e.searchInput.addEventListener("focus",(function(e){t=e.target.value,fetch(`https://restcountries.com/v3.1/name/${t}?fields=flags.svg`).then((e=>{if(!e.ok)throw new Error(e.status);return e.json()})).then((e=>e.forEach((e=>{console.log(e)}))))}));new URLSearchParams;
-//# sourceMappingURL=index.d2473424.js.map
